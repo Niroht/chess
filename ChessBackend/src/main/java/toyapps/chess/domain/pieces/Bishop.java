fa5609189a9d994d7f883c8eee200e916e4b97b6
Thorin Jacobs <thorin01@sbcglobal.net>
2019-03-13 13:31:24 -0400

    Rook movement validation

--- ChessBackend/src/main/java/toyapps/chess/domain/pieces/Bishop.java
+++ ChessBackend/src/main/java/toyapps/chess/domain/pieces/Bishop.java
@@ -4,4 +4,9 @@ public class Bishop extends ChessPiece {
     public Bishop(int x, int y) {
         super(x, y);
     }
+
+    @Override
+    public boolean canMoveToLocation(int x, int y) {
+        return false;
+    }
 }
